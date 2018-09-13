199b9225054ac9aee79c1353a5ce891e25664c0b
Adam Barton <adam.barton333@gmail.com>
2018-09-13 04:27:32 +0000

    Scraper updates

--- lib/newest_ratings/scraper.rb
+++ lib/newest_ratings/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
   end
   
   def self.scrape_products 
-    products = self.scrape_site.css(".home-table .home-tile-title").text
+    self.scrape_site.css(".home-table .home-tile-title").text.split
     # date = scrape_site.css(".home-table .silo-activity li .pub-date").text
     # date = scrape_site.css('.home-table .silo-activity li span').first.text
     # title & URL = scrape_site.css(".home-table .silo-activity li a")
@@ -18,18 +18,35 @@ class Scraper
     # text = scrape_site.css('.home-table .silo-activity li').children.children.children
     # title = Scraper.scrape_site.css('.home-table .silo-activity li a').first.text
   end
-  
-   def self.titles
+
+ def self.scrape_reviews
      titles = []
+     list = Scraper.scrape_site.css('.home-table .silo-activity ul')
+     titles << list
      
-    titles << Scraper.scrape_site.css('.home-table .silo-activity li a').text.split(":")
-     binding.pry
-    titles.each.with_index do |name, index|
-    puts "#{index} - #{name}"
-    end
-   
+    list.each.with_index do |item, index|
+        item.css('li').each do |article|
+        @name = article.css('a').text
+        @date = article.css('.pub-date').text
+        @url = article.css("a").attribute('href').value
+      # review = Review.new
+      # review.name = @name
+      # review.date = @date
+      # review.url = @url
+      # review.product = @product
+      # review.save
+      # puts "#{number + 1}:" 
+      puts self.scrape_products[index]
+      puts "#{@product}"
+      puts "#{@date}"
+      puts "#{@name}"
+      puts " "
+      puts "https://www.rtings.com#{@url}"
+      puts " "
+      end
+    end  
   end
-  
+  binding.pry
 end
 
-Scraper.titles
+Scraper.scrape_reviews
