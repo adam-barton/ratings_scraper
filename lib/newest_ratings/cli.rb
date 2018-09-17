c75228a050bd12dc5ed12516e8d82eafdf2311b0
Adam Barton <adam.barton333@gmail.com>
2018-09-17 02:09:46 +0000

    cli improvements

--- lib/newest_ratings/cli.rb
+++ lib/newest_ratings/cli.rb
@@ -14,40 +14,24 @@ class  CLI
       puts " "
       Product.list_products
       puts " "
-      puts "Or type exit to leave"
       @products = Product.all
     #   selection
     # end
 
     # def selection
-      input = nil
-      while input != "exit".strip.downcase
+      # input = nil
+      # while input != "exit".strip.downcase
     #     input = gets.strip.downcase
-
-    #       case input
-    #       when "1"
-    #         @current_selection = Product.all[0]
-    #         puts "#{@current_selection.name}, good choice."
-    #         puts "Here are the latest reviews:"
-    #           Scraper.scrape_reviews
-    #         Product.list_reviews(@current_selection)
-    #         # @current_reviews
-    #         second_menu
-    #       when "2"
-    #         Launchy.open("https://www.rtings.com/")
-    #       when "3"
-    #         a = Product.new("Headphones")
-    #         a.choice
-    #       when "menu"
-    #         main_menu
-    #       # else
-    #       #   puts "Please make a valid selection"
-    #       end
-    #   end
-    # end
     
     input = gets.strip.downcase
+    
         @selected = Product.all[input.to_i - 1]
+        
+        if input.to_i > Product.all.count
+          puts "I don't understant, try again."
+          puts "----------------------------------"
+          main_menu
+      else        
         puts "#{@selected.name}, good choice."
         puts "Here are the latest reviews:"
         Scraper.scrape_reviews
@@ -55,29 +39,32 @@ class  CLI
         list_reviews(@selected)
 
         puts " "
-        puts "Enter the number of the review you'd like to read, or type exit to go back to the main menu."
+        puts "Enter the number of the review you'd like to read."
         puts " "
 
         input = gets.strip.downcase
 
-        if input.to_i > 0 
+        if input.to_i.between?(1, 5)  
           open_review(input.to_i - 1)
-        
-        # elsif input == "exit"
-        # exit
-
+          
+        elsif input.to_i > 5
+          puts "I don't understand. Try again"
+          sleep(1)
+          main_menu
         else
         puts "I don't understand. Try again"
-        end
-     end
-   end
+      end
+      end
+    end
 
     def list_reviews(product)
       Product.list_reviews(product)
     end
     
     def open_review(review)
-      Launchy.open("@selected.reviews[review].url")
+      puts"#{@selected.reviews[review].url}"
+      puts " "
+      puts " "
       puts "Would you like to see make another selection? y/n"
       
       input = gets.strip.downcase
@@ -87,6 +74,7 @@ class  CLI
       main_menu
       when "n"
       goodbye
+      end
     end
         
 
