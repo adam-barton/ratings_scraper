abf4c3fd41fe1c5e0931a0303e961bb1621efa2a
Adam Barton <adam.barton333@gmail.com>
2018-09-17 23:38:43 +0000

    cleaning out unused code

--- lib/newest_ratings/reviews.rb
+++ lib/newest_ratings/reviews.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Review
 
   attr_accessor :title, :date, :url, :product, :synopsis
@@ -12,7 +11,6 @@ class Review
     @url = url
     self.product = product
     @product.add_review(self)
-    @synopsis = synopsis
   end
   
   def self.clear
@@ -34,10 +32,6 @@ class Review
       # Product.add_review(self)
       binding.pry
     end
-  end
-  
-  def list_synopsis
-    
   end
 
 end
